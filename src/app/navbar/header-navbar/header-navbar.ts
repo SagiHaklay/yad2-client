294c54b59e72384c4094e2f8a7895ff6d9af8bdb
Sagi Haklay <sahaklay@gmail.com>
2025-11-30 17:59:17 +0200

    added header link text

--- src/app/navbar/header-navbar/header-navbar.ts
+++ src/app/navbar/header-navbar/header-navbar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-header-navbar',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './header-navbar.scss',
 })
 export class HeaderNavbar {
-
+  profileLinkText = signal('התחברות');
+  userAlias = signal('');
 }
