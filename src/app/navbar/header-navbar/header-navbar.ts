1a45b2dbc20fde4f4b8b83aad7eda07046f6e2dd
Sagi Haklay <sahaklay@gmail.com>
2025-12-11 13:18:38 +0200

    header design

--- src/app/navbar/header-navbar/header-navbar.ts
+++ src/app/navbar/header-navbar/header-navbar.ts
@@ -3,10 +3,14 @@ import { UserProfileService } from '../../profile/user-profile-service';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ProfileIcon } from "../../profile/profile-icon/profile-icon";
 import { FavoriteService } from '../../profile/favorites/favorite-service';
+import { ActivatedRoute, RouterLink } from "@angular/router";
+import { AuthService } from '../../auth/auth-service';
+import { of, switchMap } from 'rxjs';
+import { NavbarType } from '../navbar-type';
 
 @Component({
   selector: 'app-header-navbar',
-  imports: [ProfileIcon],
+  imports: [ProfileIcon, RouterLink],
   providers: [],
   templateUrl: './header-navbar.html',
   styleUrl: './header-navbar.scss',
@@ -14,6 +18,12 @@ import { FavoriteService } from '../../profile/favorites/favorite-service';
 export class HeaderNavbar {
   private profileService = inject(UserProfileService);
   private favoriteService = inject(FavoriteService);
+  private authService = inject(AuthService);
+  private route = inject(ActivatedRoute);
+  private data = toSignal(this.route.data, {
+    initialValue: {navbarType: NavbarType.Default}
+  });
+  isProfileNavbar = computed(() => this.data().navbarType === NavbarType.Profile);
   currentUser = toSignal(this.profileService.currentUser$, {
     initialValue: null
   });
@@ -26,7 +36,19 @@ export class HeaderNavbar {
   });
   favoriteCount = this.favoriteService.favoriteCount;
   sidebarOpened = output();
+  private favorites$ = this.profileService.currentUser$.pipe(switchMap((user) => {
+    if (user) {
+      return this.favoriteService.getFavorites(this.authService.currentUserId() as number);
+    }
+    return of([]);
+  }));
+  favorites = toSignal(this.favorites$, {
+    initialValue: []
+  });
   openSidebar() {
     this.sidebarOpened.emit();
+  }
+  logout() {
+    this.authService.logout();
   }
 }
