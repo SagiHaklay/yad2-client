181dcfc91a77d8863dc1be7ef0d028b4b367c174
Sagi Haklay <sahaklay@gmail.com>
2026-01-13 08:14:17 +0200

    implement range slider

--- src/app/real-estate/real-estate-search/range-slider/range-slider.ts
+++ src/app/real-estate/real-estate-search/range-slider/range-slider.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject, signal } from '@angular/core';
+import { Component, computed, inject, input, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
@@ -10,13 +10,80 @@ import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 export class RangeSlider {
   private fb = inject(FormBuilder);
   
-  minValue = this.fb.control(0);
-  maxValue = this.fb.control(20000);
-  rangeSize = 20000;
+  minValue = input(0);
+  maxValue = input(20000);
+  rangeControls = this.fb.group({
+    min: [this.minValue()],
+    max: [this.maxValue()],
+    minText: [''],
+    maxText: ['']
+  });
+  rangeSize = computed(() => this.maxValue() - this.minValue());
+  get rangeMin() {
+    return this.rangeControls.get('min');
+  }
+  get rangeMax() {
+    return this.rangeControls.get('max');
+  }
+  get minText() {
+    return this.rangeControls.get('minText');
+  }
+  get maxText() {
+    return this.rangeControls.get('maxText');
+  }
   get minPercent() {
-    return `${(this.minValue.value || 0) / this.rangeSize * 100}%`;
+    return (this.rangeMin?.value || 0) / this.rangeSize() * 100;
   }
   get maxPercent() {
-    return `${(this.maxValue.value || 0) / this.rangeSize * 100}%`;
+    return (this.rangeMax?.value || 0) / this.rangeSize() * 100;
+  }
+  updateTextInputs() {
+    if (this.rangeMin?.value === this.minValue()) {
+      this.minText?.setValue('');
+    } else {
+      this.minText?.setValue(`${this.rangeMin?.value} ₪`);
+    }
+    if (this.rangeMax?.value === this.maxValue()) {
+      this.maxText?.setValue('');
+    } else {
+      this.maxText?.setValue(`${this.rangeMax?.value} ₪`);
+    }
+  }
+  handleMinSliderChange() {
+    const rangeMinVal = this.rangeMin?.value || 0;
+    const rangeMaxVal = this.rangeMax?.value || 0;
+    if (rangeMinVal > rangeMaxVal) {
+      this.rangeMin?.setValue(rangeMaxVal);
+    }
+    if (rangeMinVal < this.minValue()) {
+      this.rangeMin?.setValue(this.minValue());
+    }
+    this.updateTextInputs();
+  }
+  handleMaxSliderChange() {
+    const rangeMinVal = this.rangeMin?.value || 0;
+    const rangeMaxVal = this.rangeMax?.value || 0;
+    if (rangeMinVal > rangeMaxVal) {
+      this.rangeMax?.setValue(rangeMinVal);
+    }
+    if (rangeMaxVal > this.maxValue()) {
+      this.rangeMax?.setValue(this.maxValue());
+    }
+    this.updateTextInputs();
+  }
+  handleTextInput() {
+    const textMinVal = Number((this.minText?.value as string).replace(' ₪', ''));
+    const textMaxVal = Number((this.maxText?.value as string).replace(' ₪', ''));
+    if (this.minText?.value !== '') {
+      this.minText?.setValue(textMinVal + ' ₪');
+    }
+    if (this.maxText?.value !== '') {
+      this.maxText?.setValue(textMaxVal + ' ₪');
+    }
+    
+    const actualMin = textMinVal < textMaxVal? textMinVal : textMaxVal;
+    const actualMax = textMaxVal > textMinVal? textMaxVal : textMinVal;
+    this.rangeMin?.setValue(actualMin > this.minValue()? actualMin : this.minValue());
+    this.rangeMax?.setValue(actualMax < this.maxValue()? actualMax : this.maxValue());
   }
 }
